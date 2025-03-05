2c7761d0a97076259d11406b9d0b37287a913f86
lona.exe <39522664+JeswinSunsi@users.noreply.github.com>
2025-03-06 02:52:23 +0530

    Add empty Voice.vue file

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@ import { createWebHistory, createRouter } from 'vue-router'
 
 import HomeScreen from '../views/Home.vue'
 import StartScreen from '../views/Start.vue'
+import VoiceScreen from '../views/Voice.vue'
 
 const routes = [
   { path: '/', component: StartScreen },
   { path: '/home', component: HomeScreen },
+  { path: '/voice', component: VoiceScreen },
 ]
 
 const router = createRouter({
