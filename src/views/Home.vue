a277997a45f3204e1e96eaf98c21f1823a84f263
lona.exe <39522664+JeswinSunsi@users.noreply.github.com>
2025-03-06 02:48:10 +0530

    Finish creating home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,8 +11,21 @@
             <section class="all-tests-section">
                 <h2>ALL TESTS</h2>
                 <div class="test-boxes">
-                    <div class="test-box yellow"></div>
-                    <div class="test-box beige"></div>
+                    <div class="test-box" @touchstart="isTouched = 'voice'" :class="{ active: isTouched == 'voice' }">
+                        <img src="../assets/rotatedarrow.png" class="rotated-arrow" v-if="isTouched == 'voice'">
+                        <img src="../assets/voice.png" class="art">
+                        <h4 class="art-text">Voice Test</h4>
+                    </div>
+                    <div class="test-box" @touchstart="isTouched = 'posture'" :class="{ active: isTouched == 'posture' }">
+                        <img src="../assets/rotatedarrow.png" class="rotated-arrow" v-if="isTouched == 'posture'">
+                        <img src="../assets/posture.png" class="art">
+                        <h4 class="art-text">Gait Analysis</h4>
+                    </div>
+                    <div class="test-box" @touchstart="isTouched = 'hand'" :class="{ active: isTouched == 'hand' }">
+                        <img src="../assets/rotatedarrow.png" class="rotated-arrow" v-if="isTouched == 'hand'">
+                        <img src="../assets/hand.png" class="art">
+                        <h4 class="art-text">Tremor Detection</h4>
+                    </div>
                 </div>
             </section>
 
@@ -31,24 +44,25 @@
             <div class="app-footer">
                 <div class="nav-items">
                     <div class="nav-item">
-                        <div class="nav-icon profile-icon"></div>
+                        <img src="../assets/user.png" alt="Profile Icon" class="icon">
                         <div class="nav-text">Profile</div>
                     </div>
                     <div class="nav-item">
-                        <div class="nav-icon globe-icon"></div>
+                        <img src="../assets/web.png" alt="Resources Icon" class="icon">
                         <div class="nav-text">Resources</div>
                     </div>
                     <div class="nav-item">
-                        <div class="nav-icon report-icon"></div>
+                        <img src="../assets/bug.png" alt="Report Icon" class="icon">
+
                         <div class="nav-text">Report</div>
                     </div>
                     <div class="nav-item">
-                        <div class="nav-icon language-icon"></div>
+                        <img src="../assets/language.png" alt="Language Icon" class="icon">
                         <div class="nav-text">Language</div>
                     </div>
                 </div>
                 <div class="footer-credit">
-                    Made with ❤️ by Team Luminate | 2025
+                    Made with ☕ & 💖 by <span style="font-weight: 800;">Team Meltdown</span> | 2025
                 </div>
             </div>
         </div>
@@ -58,6 +72,9 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+const isTouched = ref('voice')
 </script>
 
 <style scoped>
@@ -80,7 +97,8 @@
 .title-section {
     margin-bottom: 1rem;
     padding: 1rem 0;
-    padding-top: 2.6rem;
+    padding-top: 2.4rem;
+    padding-bottom: 1.5rem;
     text-align: center;
     background-color: #D5DDBD;
 }
@@ -88,11 +106,11 @@
 h1 {
     color: #5C4231;
     font-weight: 800;
-    font-size: 24px;
+    font-size: 1.5rem;
 }
 
 .brown-box {
-    height: 26%;
+    height: 13.5rem;
     background-image: url('../assets/banner.png');
     background-size: cover;
     margin: 0 1.2rem 2.2rem 1.2rem;
@@ -114,20 +132,42 @@ h2 {
     display: flex;
     gap: 15px;
     margin-bottom: 2.2rem;
+    overflow-x: scroll;
 }
 
 .test-box {
-    width: 150px;
-    height: 150px;
+    width: 12rem;
+    min-width: 12rem;
+    height: 12rem;
     border-radius: 10px;
+    background-color: #FDECCD;
+    display: flex;
+    justify-content: end;
+    align-items: center;
+    flex-direction: column;
+    padding: 1.8rem;
+    padding-top: 0rem;
 }
 
-.yellow {
-    background-color: #ffdb99;
+.art {
+    height: 3.5rem;
+    width: auto;
 }
 
-.beige {
-    background-color: #fdf5e6;
+.art-text {
+    font-size: 0.9rem;
+    font-weight: 500;
+    color: #5C4231;
+    margin-top: 0.8rem;
+}
+
+.active {
+    background-color: #FFD384 !important;
+}
+
+.rotated-arrow {
+    padding-bottom: 2rem;
+    padding-left: 8.6rem;
 }
 
 .recent-analysis-section {
@@ -135,10 +175,10 @@ h2 {
 }
 
 .report-card {
-    background-color: #694a32;
-    padding: 1.2rem;
+    background-color: #5C4231;
+    padding: 2.2rem 1.2rem;
     border-radius: 10px;
-    color: #e8b87e;
+    color: #C19A6B;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -158,7 +198,7 @@ h2 {
 }
 
 .download-button {
-    background-color: #e8b87e;
+    background-color: #C19A6B;
     color: #694a32;
     border: none;
     padding: 0.5rem 1.2rem;
@@ -181,22 +221,20 @@ h2 {
 }
 
 .app-footer {
-    background-color: #e8ead1;
+    background-color: #D5DDBD;
     padding: 1rem 1rem;
-    border-top: 1px solid #d0d3b5;
 }
 
 .nav-items {
     display: flex;
     justify-content: space-around;
-    padding: 10px 15px;
 }
 
 .nav-item {
     display: flex;
     flex-direction: column;
     align-items: center;
-    color: #694a32;
+    color: #5C4231;
     font-size: 12px;
 }
 
@@ -209,14 +247,16 @@ h2 {
 .footer-credit {
     text-align: center;
     font-size: 12px;
-    color: #694a32;
-    margin-top: 5px;
+    color: #5C4231;
+    margin-top: 1.5rem;
+    margin-bottom: 0.5rem;
 }
 
-.profile-icon,
-.globe-icon,
-.report-icon,
-.language-icon {
-    background-color: transparent;
+.icon {
+    height: 2rem;
+    width: auto;
+    max-width: 2rem;
+    max-height: 2rem;
+    margin-bottom: 0.6rem;
 }
 </style>
