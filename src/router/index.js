6323406487876eefb45ab696aca7a2d6f5f8ea2e
lona.exe <39522664+JeswinSunsi@users.noreply.github.com>
2025-03-06 18:54:43 +0530

    Add voice to route

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,13 @@ import { createWebHistory, createRouter } from 'vue-router'
 import HomeScreen from '../views/Home.vue'
 import StartScreen from '../views/Start.vue'
 import TremorScreen from '../views/Tremor.vue'
+import VoiceScreen from '../views/Voice.vue'
 
 const routes = [
   { path: '/', component: StartScreen },
   { path: '/home', component: HomeScreen },
   { path: '/tremor', component: TremorScreen },
+  { path: '/voice', component: VoiceScreen },
 ]
 
 const router = createRouter({
