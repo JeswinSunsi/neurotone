ea599eeab3be3d618b8cfded1cde6618d5e2842f
lona.exe <39522664+JeswinSunsi@users.noreply.github.com>
2025-03-07 04:12:14 +0530

    Add write screen

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,14 @@ import HomeScreen from '../views/Home.vue'
 import StartScreen from '../views/Start.vue'
 import TremorScreen from '../views/Tremor.vue'
 import VoiceScreen from '../views/Voice.vue'
+import WriteScreen from '../views/Write.vue'
 
 const routes = [
   { path: '/', component: StartScreen },
   { path: '/home', component: HomeScreen },
   { path: '/tremor', component: TremorScreen },
   { path: '/voice', component: VoiceScreen },
+  { path: '/write', component: WriteScreen },
 ]
 
 const router = createRouter({
